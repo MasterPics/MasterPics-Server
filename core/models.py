5d0212834ff66e25b2712b7924af31152f5774b6
Subeen Park <psb021414@gmail.com>
2021-03-19 17:45:19 +0900

    [CORE] models ver.0

--- core/models.py
+++ core/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from .utils import uuid_name_upload_to, save_image_from_url
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from django.dispatch import receiver
+from allauth.account.signals import user_signed_up
+import urllib
+
+from django.shortcuts import redirect
+from user.models import User
+import re
+import json
+import datetime
+
+
+from django_mysql.models import ListCharField
+from django.utils import timezone
+
+
+class Location(models.Model):
+
+    # location
+    address = models.TextField()  # 도로명 주소
+    lat = models.FloatField(blank=True)
+    lon = models.FloatField(blank=True)
+
+    def __str__(self):
+        return self.address
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=30)
+    save_users = models.ManyToManyField(
+        to=User, related_name='tag_save_users', blank=True)
+    like_users = models.ManyToManyField(
+        to=User, related_name='tag_like_users', blank=True)
+
+    @classmethod
+    def add_tags(selt, tag_str):
+        # NOTE: self.desc 말고 TAG FIELD 따로 만들까?
+        tags = re.findall(r'#(\w+)\b', tag_str)
+        tag_lst = []
+
+        for t in tags:
+            tag, tag_created = Tag.objects.get_or_create(tag=t)
+            tag_lst.append(tag)
+
+        return tag_lst
+
+    def __str__(self):
+        return self.tag
+
+
+class Images(models.Model):
+    image = models.ImageField(
+        upload_to=uuid_name_upload_to, blank=True,null=True, verbose_name='Image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
