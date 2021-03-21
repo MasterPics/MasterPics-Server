c3430faca42c376505ff6d3d2d0ff53e6b04878e
seoljiwon <suljiwon@naver.com>
2021-03-22 03:28:03 +0900

    contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+# TODO user import 정리
+from user.models import User
 
-# Create your models here.
+import json
+# TODO location 위치 정리
+from core.models import Location
+
+from .utils import uuid_name_upload_to
+
+
+class Contact(models.Model):
+    # common field
+    user = models.ForeignKey(
+        to=User, related_name="contacts", on_delete=models.CASCADE)
+    thumbnail = models.ImageField(upload_to=uuid_name_upload_to)
+    title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    save_users = models.ManyToManyField(
+        to=User, related_name='contact_save_users', blank=True)
+    desc = models.TextField()
+    tag_str = models.CharField(max_length=50, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='contacts', blank=True)
+
+    # specific field
+    file_attach = models.FileField()
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, default=None, blank=True)
+    pay = models.PositiveIntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    is_closed = models.BooleanField(default=False)
+
+    def to_json(self):
+        return {
+            "pk": self.pk,
+            "title": self.title,
+            "pay": self.pay,
+            "start_date": self.start_date.strftime('%Y-%m-%d'),
+            "end_date": self.end_date.strftime('%Y-%m-%d'),
+            "address": self.location.address,
+            "lat": self.location.lat,
+            "lon": self.location.lon,
+        }
+
+    def classname(self):
+        return self.__class__.__name__
---
+++ contact/static/js/comment.js
@@ -0,0 +1,53 @@
+const modifyNewComment = (contact_id, comment_id, value) => {
+    const CommentContainer = document.querySelector(`.comments-${contact_id}`);
+    console.log(CommentContainer)
+
+    const tempContainer = document.createElement("div");
+    tempContainer.className = `comment comment-${comment_id}`;
+
+    tempContainer.textContent = value;
+
+    const deleteBtn = document.createElement("input");
+    deleteBtn.className = "comment-btn";
+    deleteBtn.setAttribute("type", "submit");
+    deleteBtn.setAttribute("value", "삭제");
+    deleteBtn.setAttribute("onclick", `onClickDeleteComment(${comment_id})`)
+
+    tempContainer.appendChild(deleteBtn);
+    CommentContainer.appendChild(tempContainer);
+
+}
+
+const onClickNewComment = async (id) => {
+    const url = `/contact/${id}/comment_create/`;
+    const value = document.querySelector(`.createComment-${id} .comment__value`);
+    const value_text = value.value
+    const {
+        data
+    } = await axios.post(url, {
+        id,
+        value: value_text
+    })
+
+    modifyNewComment(id, data.comment_id, data.value);
+
+    const cmt = document.querySelector('.comment__value')
+    cmt.value = ''
+}
+
+const modifyDeleteComment = (comment_id) => {
+
+    const targetCommentContainer = document.querySelector(`.comment-${comment_id}`);
+    targetCommentContainer.remove();
+}
+
+const onClickDeleteComment = async (comment_id) => {
+    const url = `/contact/${comment_id}/comment_delete/`;
+
+    const {
+        data
+    } = await axios.post(url, {
+        comment_id
+    })
+    modifyDeleteComment(data.comment_id);
+}
