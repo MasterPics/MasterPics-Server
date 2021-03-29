32e9a8f6f26445cc09ea8d8e082933f7312b4c90
seoljiwon <suljiwon@naver.com>
2021-03-30 02:46:34 +0900

    edit url

--- contact/urls.py
+++ contact/urls.py
@@ -4,18 +4,18 @@ from . import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('contact/', view=views.contact_list, name='contact_list'),
-    path('contact/map/', view=views.contact_map, name='contact_map'),
-    path('contact/detail/<int:pk>/',
+    path('', view=views.contact_list, name='contact_list'),
+    path('map/', view=views.contact_map, name='contact_map'),
+    path('detail/<int:pk>/',
          view=views.contact_detail, name='contact_detail'),
-    path('contact/<int:pk>/comment_create/',
+    path('<int:pk>/comment_create/',
          view=views.contact_comment_create, name='contact_comment_create'),
-    path('contact/<int:pk>/comment_delete/',
+    path('<int:pk>/comment_delete/',
          view=views.contact_comment_delete, name='contact_comment_delete'),
-    path('contact/<int:pk>/delete/',
+    path('<int:pk>/delete/',
          view=views.contact_delete, name='contact_delete'),
-    path('contact/<int:pk>/update/',
+    path('<int:pk>/update/',
          view=views.contact_update, name='contact_update'),
-    path('contact/create/', view=views.contact_create, name='contact_create'),
-    path('contact/save/', views.contact_save, name='contact_save'),
+    path('create/', view=views.contact_create, name='contact_create'),
+    path('save/', views.contact_save, name='contact_save'),
 ]
