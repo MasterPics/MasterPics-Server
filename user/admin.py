6868b9d48279ea4872ee5f164007482ac669a17b
jm choi <527wlals@gmail.com>
2021-04-11 01:58:33 +0900

    create user admin class (admin.py)
    django admin 페이지에서, social login한 user에 대한 정보(api를 통해 넘어와 user 객체에 저장된 정보)를 확인하기 위해 UserAdmin class 생성

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'username']
+    list_display_links = ['email', 'username']
