7f50780cf4b8c7cc35b62c93d260dd1da57e8c12
Subeen Park <psb021414@gmail.com>
2021-05-06 23:39:34 +0900

    reference model removed

--- reference/models.py
+++ reference/models.py
@@ -5,16 +5,3 @@ from user.models import User
 
 
 from .utils import uuid_name_upload_to
-
-
-# class Reference(models.Model):
-#     thumbnail = models.ImageField(upload_to=uuid_name_upload_to)
-#     save_users = models.ManyToManyField(
-#         to=User, related_name='reference_save_users', blank=True)
-#     like_users = models.ManyToManyField(
-#         to=User, related_name='reference_like_users', blank=True)
-#     desc = models.TextField()
-#     image_url = ListCharField(base_field=models.CharField(
-#         max_length=100), max_length=60000)
-#     tag_str = models.CharField(max_length=50, blank=True)
-#     tags = models.ManyToManyField(Tag, related_name='references', blank=True)
