576187a787dd924080b72ea4bd7138a8f70e70fc
HoYoungChun <ho723@naver.com>
2021-05-06 22:13:42 +0900

    다중이미지 구현

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -4,6 +4,11 @@ from .models import *
 # Register your models here.
 
 
+@admin.register(Images)
+class PortfolioAdmin(admin.ModelAdmin):
+    pass
+
+
 @admin.register(Portfolio)
 class PortfolioAdmin(admin.ModelAdmin):
     pass
@@ -12,3 +17,13 @@ class PortfolioAdmin(admin.ModelAdmin):
 # @admin.register(ViewCount)
 # class ViewCountAdmin(admin.ModelAdmin):
 #     pass
+
+class Images(admin.TabularInline):
+    model = Images
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        for img in request.FILES.getlist('photos'):
+            obj.productimage_set.create(image_url=img)
