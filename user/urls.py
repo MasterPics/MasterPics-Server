4e5ce6d419932794bed2563a144502ec23fa77f2
jm choi <527wlals@gmail.com>
2021-05-08 23:19:17 +0900

    delete existing user app code
    회원 탈퇴는 일단 남겨놓음 (profile_delete)

--- user/urls.py
+++ user/urls.py
@@ -4,23 +4,13 @@ from . import views
 app_name = 'profile'
 
 urlpatterns = [
-    path('<int:pk>/', view=views.profile_detail, name='profile_detail'),
-    path('<int:pk>/posts/',
-         view=views.profile_detail_posts, name='profile_detail_posts'),
-     path('<int:pk>/saves/',
-         view=views.profile_detail_saves, name='profile_detail_saves'),
     path('<int:pk>/delete/',
          view=views.profile_delete, name='profile_delete'),
-    path('<int:pk>/update/',
-         view=views.profile_update, name='profile_update'),
-    path('<int:pk>/profile_update_password/',
-         view=views.profile_update_password, name='profile_update_password'),
-    path('create/', view=views.profile_create, name='profile_create'),
-    path('post/create/', view=views.post_create, name='post_create'),
 #     ------------------------new------------------------------
      path('local_signup/', view=views.local_signup, name='local_signup'),
      path('login/', view=views.login, name='login'),
      path('logout/', view=views.logout, name='logout'),
+     path('profile/', view=views.profile, name='profile'),
      path('profile_portfolio/', view=views.profile_portfolio, name='profile_portfolio'),
      path('profile_post_contact/', view=views.profile_post_contact, name='profile_post_contact'),
      path('profile_post_tagged/', view=views.profile_post_tagged, name='profile_post_tagged'),
--- user/views.py
+++ user/views.py
@@ -25,143 +25,6 @@ def profile_delete(request, pk):
         return render(request, 'profile/profile_delete.html', context=ctx)
 
 
-@login_required
-def profile_update(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES, instance=user)
-        if form.is_valid():
-            print("form.is_valid")
-
-            # user = form.save()
-            # if user.image:
-            #     user.image = request.FILES.get('image')
-            # return redirect('myApp:profile_detail', user.id)
-            user = form.save()
-            user.image = request.FILES.get('image')
-
-            return redirect('profile_detail', user.id)
-
-    else:
-        form = ProfileForm(instance=user)
-        ctx = {'form': form}
-        return render(request, 'profile/profile_update.html', ctx)
-
-
-@login_required
-def profile_update_password(request, pk):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)  # Important!
-            messages.success(
-                request, 'Your password was successfully updated!')
-            return redirect('change_password')
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'profile/profile_update_password.html', {
-        'form': form
-    })
-
-
-def profile_create(request):
-    # if request.user.is_authenticated:
-    #     return redirect('myApp:profile_detail')
-
-    if request.method == 'POST':
-        signup_form = ProfileForm(request.POST, request.FILES)
-        if signup_form.is_valid():
-            user = signup_form.save()
-            user.image = request.FILES['image']
-
-            # automatic login
-            auth_login(request, user,
-                       backend='django.contrib.auth.backends.ModelBackend')
-
-            return redirect('profile:profile_detail', user.id)
-
-    else:
-        signup_form = ProfileForm()
-
-    return render(request, 'profile/profile_create.html', {'form': signup_form})
-
-
-@login_required
-def profile_detail(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    request_user=request.user
-    ctx = {'user': user,'request_user':request_user, }
-    return render(request, 'profile/profile_detail.html', context=ctx)
-
-
-@login_required
-def profile_detail_posts(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    request_user=request.user
-
-    category = request.GET.get('category', 'contact')  # CATEGORY
-    # sort = request.GET.get('sort', 'recent')  # SORT
-    # search = request.GET.get('search', '')  # SEARCH
-
-    # CATEGORY
-    if category == 'contact':
-        posts = user.contacts.all()
-
-    elif category == 'portfolio':
-        posts = user.portfolios.all()
-
-    # SORT
-
-    # SEARCH
-
-    # infinite scroll
-
-    ctx = {
-        'user': user,
-        'posts': posts,
-        'category': category,
-    }
-    return render(request, 'profile/profile_detail_posts.html', context=ctx)
-
-
-@login_required
-def profile_detail_saves(request, pk):
-    user = get_object_or_404(User, pk=pk)
-    request_user=request.user
-
-    category = request.GET.get('category', 'contact')  # CATEGORY
-    # sort = request.GET.get('sort', 'recent')  # SORT
-    search = request.GET.get('search', '')  # SEARCH
-
-    # CATEGORY
-    if category == 'contact':
-        posts = Contact.objects.all()
-    elif category == 'portfolio':
-        posts = Portfolio.objects.all()
-
-    ctx = {
-        'user': user,
-        'posts': posts,
-        'category': category,
-        'request_user':request_user,
-    }
-    return render(request, 'profile/profile_detail_saves.html', context=ctx)
-
-
-@login_required
-def post_create(request):
-    if request.method == 'POST':
-        messages.success(request, "create your post!")
-    else:
-        ctx = {}
-        return render(request, 'profile/post_create.html', context=ctx)
-
-
-
-
 # ----------------------new-------------------------------
 from .forms import SignupForm, LoginForm, ProfileModifyForm, LocalPasswordChangeForm, SocialUserInfoForm
 from django.contrib.auth import authenticate
@@ -247,6 +110,9 @@ def social_user_more_info(request):
 # ----mypage 관련----
 # TODO : 현재는 모든 유저가 자신의 프로필만 볼 수 있게 되어있음 -> 다른 사람의 프로필도 볼 수 있게 고치기
 #         -> 파라미터에 user_identifier 추가, profile_owner 바꾸기
+def profile(request):
+    return redirect('profile:profile_portfolio')
+
 def profile_portfolio(request):
     profile_owner = request.user
     portfolios = profile_owner.portfolios.all()
@@ -334,7 +200,7 @@ def profile_save(request):
 # ----profile 관련----
 def profile_modify(request):
     if request.method == 'POST':
-        form = ProfileModifyForm(request.POST, instance=request.user)
+        form = ProfileModifyForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
             return redirect('profile:profile_portfolio')
