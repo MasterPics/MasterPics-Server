37098969821f5b0ae99fcd597a4c61131f0df889
jm choi <527wlals@gmail.com>
2021-07-11 04:08:52 +0900

    add recovery_pw urls

--- user/urls.py
+++ user/urls.py
@@ -27,4 +27,11 @@ urlpatterns = [
      # smpt
      path('smtp_sending_success/', view=views.smtp_sending_success, name='smtp_sending_success'),
      path('smtp_auth/<str:uid64>/<str:token>/', view=views.smtp_auth, name='smtp_auth'),
+
+     # 비밀번호 인증관련
+     path('recovery/pw/', view=views.recovery_pw, name='recovery_pw'),
+     path('recovery/pw/find/', view=views.recovery_pw_send_email, name='ajax_pw'),
+     path('recovery/pw/auth/', view=views.recovery_pw_auth_confirm, name='recovery_auth'),
+     path('recovery/pw/reset/', view=views.recovery_pw_reset, name='recovery_pw_reset'),
+
 ]
