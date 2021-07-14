75f9b2c3f8706e42bba293abe45c7fae6e3c85e6
seoljiwon <suljiwon@naver.com>
2021-07-14 15:30:35 +0900

    refactor: remove console.log for debugging

--- portfolio/static/js/infinite.scroll.js
+++ portfolio/static/js/infinite.scroll.js
@@ -1,12 +1,11 @@
 let infinite = new Waypoint.Infinite({
+
     element: $('.grid')[0],
     items: '.grid-item',
     onBeforePageLoad: function () {
-        console.log("here here")
         $('.loading').show();
     },
     onAfterPageLoad: function () {
-        console.log("here here2")
         $('.loading').hide();
         let msnry = new Masonry('.grid', {
             itemSelector: '.grid-item',
@@ -14,8 +13,6 @@ let infinite = new Waypoint.Infinite({
         })
     }
 });
-
-
 let msnry = new Masonry('.grid', {
     // optionsW
     itemSelector: '.grid-item',
