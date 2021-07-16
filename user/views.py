885249b6d3f5e602a8370ab6e6292c2dd8aae277
jm choi <527wlals@gmail.com>
2021-07-17 03:05:06 +0900

    modify mypage-bookmark (ajax)

--- user/views.py
+++ user/views.py
@@ -10,6 +10,9 @@ import hashlib
 
 # ----------------------new-------------------------------
 from .forms import SignupForm, LoginForm, ProfileModifyForm, LocalPasswordChangeForm, SocialUserInfoForm
+from portfolio.models import Portfolio, Participants
+from contact.models import Contact
+from place.models import Place
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
@@ -250,108 +253,59 @@ def mypage_post_place(request):
     return JsonResponse({"places": places})
 
 
+
 # mypage / 저장 목록 / 포트폴리오
+@csrf_exempt
 def mypage_bookmark_portfolio(request):
-    mypage_owner = request.user
-    # bookmarked_informations = mypage_owner.save_users.all()      # mypage_owner가 bookmark한 information 객체들
-    # bookmarked_portfolios = []        # mypage_owner bookmark한 portfolio 객체들
-    # for info in bookmarked_informations:
-    #     bookmarked_portfolios.append(info.portfolioInformations.portfolio)  # info가 portfolioInformation이 아닌 다른 곳(contactInformation, placeInformation)에
-    #                                                                         # 연결되어있으면 오류 발생. portfolioInformations가 없다고 나옴
-
-    # TODO : manyTomany 쿼리 필터 공부하기 -> 코드 효율적으로 수정 필요
-    bookmarked_informations = Information.objects.filter(
-        save_users=mypage_owner)     # mypage_owner가 bookmark한 information 객체들
-    portfolioInformations = PortfolioInformation.objects.all(
-    )                          # 모든 portfolioInformation 객체들
-    # mypage_owner가 bookmark한 portfolio 객체들
+    bookmarked_portfolios_query = Portfolio.objects.filter(bookmark_users=request.user)
     bookmarked_portfolios = []
-    for portfolioInformation in portfolioInformations:
-        for information in bookmarked_informations:
-            if portfolioInformation.information == information:
-                bookmarked_portfolios.append(portfolioInformation.portfolio)
-
-    portfolio_count = mypage_owner.portfolios.count()
-    contact_count = mypage_owner.contacts.count()
-
-    ctx = {
-        'mypage_owner': mypage_owner,
-        'bookmarked_portfolios': bookmarked_portfolios,
-        'portfolio_count': portfolio_count,
-        'contact_count': contact_count,
-    }
-
-    return render(request, 'profile/mypage_bookmark_portfolio.html', ctx)
+    for portfolio in bookmarked_portfolios_query:
+        bookmarked_portfolios.append({
+            'id': portfolio.id,
+            'title': portfolio.title,
+            'like_count': portfolio.like_users.count(),
+            'view_count': portfolio.view_count,
+            'thumbnail_url': portfolio.thumbnail.image.url,
+            'is_bookmark': True
+        })
+    
+    return JsonResponse({"bookmarked_portfolios": bookmarked_portfolios})
 
 
 # mypage / 저장 목록 / 컨택트
-
-
+@csrf_exempt
 def mypage_bookmark_contact(request):
-    mypage_owner = request.user
-    # bookmarked_informations = mypage_owner.save_users.all()
-    # bookmarked_contacts = []
-    # for info in bookmarked_informations:
-    #     bookmarked_contacts.append(info.contactInformations.contact)
-
-    # TODO : manyTomany 쿼리 필터 공부하기 -> 코드 효율적으로 수정 필요
-    bookmarked_informations = Information.objects.filter(
-        save_users=mypage_owner)     # mypage_owner가 bookmark한 information 객체들
-    contactInformations = ContactInformation.objects.all(
-    )                          # 모든 portfolioInformation 객체들
-    # mypage_owner가 bookmark한 portfolio 객체들
+    bookmarked_contacts_query = Contact.objects.filter(bookmark_users=request.user)
     bookmarked_contacts = []
-    for contactInformation in contactInformations:
-        for information in bookmarked_informations:
-            if contactInformation.information == information:
-                bookmarked_contacts.append(contactInformation.contact)
-
-    portfolio_count = mypage_owner.portfolios.count()
-    contact_count = mypage_owner.contacts.count()
-
-    ctx = {
-        'mypage_owner': mypage_owner,
-        'bookmarked_contacts': bookmarked_contacts,
-        'portfolio_count': portfolio_count,
-        'contact_count': contact_count,
-    }
-
-    return render(request, 'profile/mypage_bookmark_contact.html', ctx)
+    for contact in bookmarked_contacts_query:
+        bookmarked_contacts.append({
+            'id': contact.id,
+            'title': contact.title,
+            'like_count': contact.like_users.count(),
+            'view_count': contact.view_count,
+            'thumbnail_url': contact.thumbnail.image.url,
+            'is_bookmark': True
+        })
+    
+    return JsonResponse({"bookmarked_contacts": bookmarked_contacts})
 
 
 # mypage / 저장 목록 / 플레이스
-
-
+@csrf_exempt
 def mypage_bookmark_place(request):
-    mypage_owner = request.user
-    # bookmarked_informations = mypage_owner.save_users.all()
-    # bookmarked_contacts = []
-    # for info in bookmarked_informations:
-    #     bookmarked_contacts.append(info.contactInformations.contact)
-
-    # TODO : manyTomany 쿼리 필터 공부하기 -> 코드 효율적으로 수정 필요
-    bookmarked_informations = Information.objects.filter(
-        save_users=mypage_owner)     # mypage_owner가 bookmark한 information 객체들
-    # 모든 portfolioInformation 객체들
-    placeInformations = PlaceInformation.objects.all()
-    # mypage_owner가 bookmark한 portfolio 객체들
+    bookmarked_places_query = Contact.objects.filter(bookmark_users=request.user)
     bookmarked_places = []
-    for placeInformation in placeInformations:
-        for information in bookmarked_informations:
-            if placeInformation.information == information:
-                bookmarked_places.append(placeInformation.place)
-
-    portfolio_count = mypage_owner.portfolios.count()
-    contact_count = mypage_owner.contacts.count()
-
-    ctx = {
-        'mypage_owner': mypage_owner,
-        'bookmarked_places': bookmarked_places,
-        'portfolio_count': portfolio_count,
-        'contact_count': contact_count,
-    }
-
-    return render(request, 'profile/mypage_bookmark_place.html', ctx)
+    for place in bookmarked_places_query:
+        bookmarked_places.append({
+            'id': place.id,
+            'title': place.title,
+            'like_count': place.like_users.count(),
+            'view_count': place.view_count,
+            'thumbnail_url': place.thumbnail.image.url,
+            'is_bookmark': True
+        })
+    
+    return JsonResponse({"bookmarked_places": bookmarked_places})
 
 
 # ----profile 관련----
