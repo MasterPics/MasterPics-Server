db923ec42f29c97ac81dce5b8d773ed25ccf3695
seoljiwon <suljiwon@naver.com>
2021-07-25 04:53:27 +0900

    feat: mypage saved_post

---
+++ user/static/js/mypage_saved_place.js
@@ -0,0 +1,62 @@
+const onClickSavedPlace = async(user_id) => {
+    try{
+        const options = {
+            url: '/profile/mypage/bookmark/place/',
+            method: 'POST',
+            data: {
+                user_id: user_id,
+            }
+        }
+    const response = await axios(options)
+    const responseOK = response && response.status === 200 && response.statusText === 'OK'
+    if (responseOK) {
+        const data = response.data
+        listSavedPlace(data.bookmarked_places)
+    }
+    } catch (error) {
+        console.log(error)
+    }
+}
+const listSavedPlace = (bookmarked_places) => {
+    // local-bar 변경
+    const postSavedPortfolio= document.querySelector('#post__saved-portfolio');
+    const postSavedContact = document.querySelector('#post__saved-contact');
+    const postSavedPlace = document.querySelector('#post__saved-place');
+    postSavedPortfolio.className = "";
+    postSavedContact.className = "";
+    postSavedPlace.className = "post__local-sub-bar-selected";
+
+    // 게시글 변경
+    const postContainer = document.querySelector('.post__container');
+    postContainer.innerHTML='';
+    for(let i=0; i<bookmarked_places.length; i++){
+        postContainer.innerHTML+=`<div class="post__item">`+
+            `<figure class="post__image">`+
+                `<img src=${bookmarked_places[i].thumbnail_url}>`+
+                `<figcaption>`+
+                    `<div class="post__info">`+
+                        `<p class="post__title">${bookmarked_places[i].title}</p>`+
+                        `<div>`+
+                            `<p class="post__comment">`+
+                                `<i class="far fa-comment-dots"></i>`+
+                                `<span>${bookmarked_places[i].comment_count}</span>`+
+                            `</p>`+
+                            `<p class="post__bookmark save save-${bookmarked_places[i].id}">`+
+                            `</p>`+
+                        `</div>`+
+                    `</div>`+
+                `</figcaption>`+
+            `</figure>`+
+        `</div>`
+    }
+
+    // save 기능
+    for(let i=0; i<bookmarked_places.length; i++){
+        const save = document.querySelector(`.save-${bookmarked_places[i].id}`);
+        if( bookmarked_places[i].is_bookmark === true ){
+            save.innerHTML = `<i class="fas fa-bookmark" type="submit" onclick="onClickPlaceSave(${bookmarked_places[i].id})" name="type" value="save"></i>`
+        } else {
+            save.innerHTML = `<i class="far fa-bookmark" type="submit" onclick="onClickPlaceSave(${bookmarked_places[i].id})" name="type" value="save"></i>`
+        }
+    }
+}
---
+++ user/static/js/mypage_saved_post.js
@@ -0,0 +1,78 @@
+const onClickSavedPost = async(user_id) => {
+    try{
+        const options = {
+            url: '/profile/mypage/bookmark/portfolio/',
+            method: 'POST',
+            data: {
+                user_id: user_id,
+            }
+        }
+    const response = await axios(options)
+    const responseOK = response && response.status === 200 && response.statusText === 'OK'
+    if (responseOK) {
+        const data = response.data
+        listSavedPost(data.bookmarked_portfolios)
+    }
+    } catch (error) {
+        console.log(error)
+    }
+}
+const listSavedPost = (bookmarked_portfolios) => {
+    // local-bar 변경
+    const postPortfolio = document.querySelector('#post__portfolio');
+    const postMyPost = document.querySelector('#post__my-post');
+    const postSavedPost = document.querySelector('#post__saved-post');
+    const postPortfolioSelected = document.querySelector('#post__portfolio-selected');
+    const postMyPostSelected = document.querySelector('#post__my-post-selected');
+    const postSavedPostSelected = document.querySelector('#post__saved-post-selected');
+    const postSavedPortfolio= document.querySelector('#post__saved-portfolio');
+    const postSavedContact = document.querySelector('#post__saved-contact');
+    const postSavedPlace = document.querySelector('#post__saved-place');
+    postPortfolio.className = "";
+    postMyPost.className = "";
+    postSavedPost.className = "post__local-bar-selected";
+    postPortfolioSelected.className = "post__not-selected";
+    postMyPostSelected.className = "post__not-selected";
+    postSavedPostSelected.className = "";
+    postSavedPortfolio.className = "post__local-sub-bar-selected";
+    postSavedContact.className = "";
+    postSavedPlace.className = "";
+
+    // 게시글 변경
+    const postContainer = document.querySelector('.post__container');
+    postContainer.innerHTML='';
+    for(let i=0; i<bookmarked_portfolios.length; i++){
+        postContainer.innerHTML+=`<div class="post__item">`+
+            `<figure class="post__image">`+
+                `<img src=${bookmarked_portfolios[i].thumbnail_url}>`+
+                `<figcaption>`+
+                    `<div class="post__info">`+
+                        `<p class="post__title">${bookmarked_portfolios[i].title}</p>`+
+                        `<div>`+
+                            `<p class="post__like">`+
+                                `<i class="fas fa-heart"></i>`+
+                                `<span>${bookmarked_portfolios[i].like_count}</span>`+
+                            `</p>`+
+                            `<p class="post__view">`+
+                                `<i class="far fa-eye"></i>`+
+                                `<span>${bookmarked_portfolios[i].view_count}</span>`+
+                            `</p>`+
+                            `<p class="post__bookmark save save-${bookmarked_portfolios[i].id}">`+
+                            `</p>`+
+                        `</div>`+
+                    `</div>`+
+                `</figcaption>`+
+            `</figure>`+
+        `</div>`
+    }
+
+    // save 기능
+    for(let i=0; i<bookmarked_portfolios.length; i++){
+        const save = document.querySelector(`.save-${bookmarked_portfolios[i].id}`);
+        if( bookmarked_portfolios[i].is_bookmark === true ){
+            save.innerHTML = `<i class="fas fa-bookmark" type="submit" onclick="onClickPortfolioSave(${bookmarked_portfolios[i].id})" name="type" value="save"></i>`
+        } else {
+            save.innerHTML = `<i class="far fa-bookmark" type="submit" onclick="onClickPortfolioSave(${bookmarked_portfolios[i].id})" name="type" value="save"></i>`
+        }
+    }
+}
