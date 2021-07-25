e8dc55559063bd054904eeeec8b6bc68e78fed43
chaewon <lisa9902@naver.com>
2021-07-25 12:12:32 +0900

    sort style, delete

--- place/forms.py
+++ place/forms.py
@@ -9,8 +9,7 @@ class PlaceForm(forms.ModelForm):
     class Meta:
         model = Place
         fields = ('title', 'pay', 'tags', 'desc')
-        # 순서 변경
-        # 동시에 상호무페이랑 pay를 처리하는 것 보다는 전에 했던 대로 가격이 0인 경우 상호무페이 처리가 간결한 것 같아서 다시 수정했습니다.
+        # 순서 변경ㅋ
         widgets = {
             'title': forms.TextInput(attrs={'placeholder': '제목을 입력하세요.'}),
             'pay': forms.NumberInput(attrs={'placeholder': '페이를 입력하세요.(상호 무페이의 경우 0을 입력하세요)'}),
