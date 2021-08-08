228e973bf89ce8b8d665299f2d23145b08f47b7b
Subeen Park <psb021414@gmail.com>
2021-08-09 01:04:06 +0900

    fix: contact PayType wrong variable name

--- contact/models.py
+++ contact/models.py
@@ -7,14 +7,10 @@ import json
 from django.shortcuts import get_object_or_404
 
 
-PAY_CUSTOM = 0
-PAY_NEGO = 1
-PAY_FREE = 2
-
 class PayType(models.IntegerChoices):
-    PAY_CUSTOM = 0, '페이 입력'
-    PAY_NEGO = 1, '상호 무페이'
-    PAY_FREE = 2, '페이 협의'
+    PAY_FREE = 0, '상호 무페이'
+    PAY_CUSTOM = 1, '페이 입력'
+    PAY_NEGO = 2, '페이 협의'
 
 
 class Contact(PostBase):
@@ -26,7 +22,7 @@ class Contact(PostBase):
     location = models.ForeignKey(
         to=Location, on_delete=models.CASCADE, default=None, blank=True)
     pay = models.PositiveIntegerField(blank=True, default=0)
-    pay_type = models.IntegerField(choices=PayType.choices, default=PayType.PAY_CUSTOM)
+    pay_type = models.IntegerField(choices=PayType.choices, default=PayType.PAY_FREE)
 
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
