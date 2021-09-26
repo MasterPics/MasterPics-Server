92a26eb3781d1b25bce06da87317d413ec460bcf
seol <suljiwon@naver.com>
2021-09-27 00:29:43 +0900

    fix: show contact filename

--- contact/static/js/contact_create_file_prev.js
+++ contact/static/js/contact_create_file_prev.js
@@ -1,11 +1,11 @@
-window.onload = function(){
-    target = document.querySelector('.file_attach .form');
-    target.addEventListener('change', function(){
-        fileList = "";
-        for(i = 0; i < target.files.length; i++){
-            fileList += target.files[i].name + '<br>';
-        }
-        target2 = document.getElementById('showFiles');
-        target2.innerHTML = "파일명: " + fileList;
-    });
-}
+// window.onload = function () {
+target = document.querySelector('.file_attach .form');
+target.addEventListener('change', function () {
+    let fileList = "";
+    for (i = 0; i < target.files.length; i++) {
+        fileList += target.files[i].name + '<br>';
+    }
+    target2 = document.getElementById('showFiles');
+    target2.innerHTML = "파일명: " + fileList;
+});
+// }
--- contact/static/js/contact_update_file_prev.js
+++ contact/static/js/contact_update_file_prev.js
@@ -1,15 +1,15 @@
-window.onload = function(){
-    target = document.querySelector('.file_attach .form');
-    target.addEventListener('change', function(){
-        fileList = "";
-        for(i = 0; i < target.files.length; i++){
-            fileList += target.files[i].name + '<br>';
-        }
-        target2 = document.getElementById('showFiles');
-        target2.innerHTML = "변경: " + fileList;
-    });
-    var cur = document.querySelector('.file_attach .form').previousSibling.previousSibling.previousSibling
+// window.onload = function(){
+target = document.querySelector('.file_attach .form');
+target2 = document.getElementById('showFiles');
+target.addEventListener('change', function () {
+    let fileList = "";
+    for (i = 0; i < target.files.length; i++) {
+        fileList += target.files[i].name + '<br>';
+    }
+    target2.innerHTML = "변경: " + fileList;
+});
+var cur = document.querySelector('.file_attach .form').parentElement.firstChild.nextSibling
 
-    target2 = document.getElementById('showPrevFiles');
-    target2.innerHTML = "현재: " + cur.textContent;
-}
+// target2 = document.getElementById('showPrevFiles');
+target2.innerHTML = "현재: " + cur.textContent;
+// }
