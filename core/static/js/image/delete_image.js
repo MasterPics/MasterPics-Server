401f71a6c7eaa731fc9cb685e8ce66cd3ca1bafd
seol <suljiwon@naver.com>
2021-09-26 19:01:10 +0900

    fix: alert when only one image left

--- core/static/js/image/delete_image.js
+++ core/static/js/image/delete_image.js
@@ -2,8 +2,13 @@ delete_list = [] //삭제할 이미지들 누적
 
 function remove(el) {
     var element = el;
-    delete_list.push(el.parentNode.classList[1]) //div의 class명이 삭제할 이미지의 pk
-    element.parentNode.remove(); //div 제거
+    if (el.parentNode.parentNode.children.length == 1) { //이거 지우면 이미지 다 없어질때
+        alert("이미지는 한 개 이상 포함해야 합니다.");
+    }
+    else {
+        delete_list.push(el.parentNode.classList[1]) //div의 class명이 삭제할 이미지의 pk
+        element.parentNode.remove(); //div 제거
+    }
 }
 
 function remove_all() {
